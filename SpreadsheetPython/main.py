97adf76bc486b748f3a084f8990d3d0de123f519
Willette <aawill@umhs.med.umich.edu>
2018-04-25 15:59:25 -0400

    initial py setup

--- SpreadsheetPython/main.py
+++ SpreadsheetPython/main.py
@@ -1,3 +1,9 @@
 import csv
 
-print 'Hello World'
+with open('2017 Retina Podcast Analytics.csv', 'r') as csvFile:
+    csvReader = csv.reader(csvFile)
+
+    next(csvReader)
+
+    for line in csvReader:
+        print line[3]
